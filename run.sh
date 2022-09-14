e100edcdbeb6904611a8d51f52bba26337697060
Jagoda Kamińska <jk406179@students.mimuw.edu.pl>
2022-09-15 00:32:45 +0200

    switch from docker stack(swarm+compose) to ansible+docker build/run

--- run.sh
+++ run.sh
@@ -20,43 +20,10 @@ cd ansible
 
 echo "Setup docker and docker swarm on all nodes:"
 ansible-playbook -i ./hosts --extra-vars "ansible_user=st105 ansible_password=$1" docker-playbook.yaml
-ansible-playbook -i ./hosts --extra-vars "ansible_user=st105 ansible_password=$1" swarm-playbook.yaml
 echo "Done"
 
-echo "Setup aerospike on chosen nodes:"
-ansible-playbook -i ./hosts --extra-vars "ansible_user=st105 ansible_password=$1" aerospike-playbook.yaml
-echo "Done"
-
-echo "Setup kafka and zookeeper on chosen nodes:"
-ansible-playbook -i ./hosts --extra-vars "ansible_user=st105 ansible_password=$1" kafka-playbook.yaml
+echo "Start allezone:"
+ansible-playbook -i ./hosts --extra-vars "ansible_user=st105 ansible_password=$1" startup-playbook.yaml
 echo "Done"
 
 cd ..
-
-echo "Create local docker registry..."
-sudo docker service create --name registry -p 5000:5000 registry:2
-echo "Done"
-
-echo "Set current node as haproxy node..."
-sudo docker node update --label-add haproxy=true $(hostname)
-echo "Done"
-
-#echo "Install and set up weavenet docker plugin..."
-#sudo docker plugin install weaveworks/net-plugin:latest_release
-#sudo docker plugin disable weaveworks/net-plugin:latest_release
-#sudo docker plugin set weaveworks/net-plugin:latest_release WEAVE_MULTICAST=1
-#sudo docker plugin enable weaveworks/net-plugin:latest_release
-#echo "Done"
-
-echo "Build docker services..."
-sudo docker-compose build
-echo "Done"
-
-echo "Push built images to local registry..."
-sudo docker-compose push
-echo "Done"
-
-echo "Deploy allezone..."
-sudo docker stack deploy --compose-file docker-compose.yml allezone 
-echo "Done"
-
