56cbee0eb099e00f66efa35d6f72647911bedd8c
Jagoda Kamińska <jk406179@students.mimuw.edu.pl>
2022-09-14 16:57:52 +0200

    fixes

---
+++ app_aggregator/src/config.py
@@ -0,0 +1,5 @@
+kafka_hosts = ['app_kafka:9092']
+aerospike_hosts = [('app_aerospike', 3000)]
+kafka_topic = "user_tags"
+
+# #frontend_hosts/bucket_master_hosts <= #kafka_hosts/partitions
---
+++ app_aggregator/src/custom_types.py
@@ -0,0 +1,63 @@
+from typing import Union, List
+from pydantic import BaseModel, Field, validator
+from enum import Enum
+from dateutil.parser import parse
+from datetime import datetime
+
+class Device(str, Enum):
+    PC = "PC"
+    MOBILE = "MOBILE"
+    TV = "TV"
+
+class Action(str, Enum):
+    VIEW = "VIEW"
+    BUY = "BUY"
+
+class Aggregate(str, Enum):
+    COUNT = "COUNT"
+    SUM_PRICE = "SUM_PRICE"
+
+class ProductInfo(BaseModel):
+    product_id: str
+    brand_id: str
+    category_id: str
+    price: int = Field(ge=0)
+
+class UserTag(BaseModel):
+    time: str
+    cookie: str
+    country: str
+    device: Device
+    action: Action
+    origin: str
+    product_info: ProductInfo
+
+    @validator('time')
+    # TODO: it doesn't validate 2022-03-22T12:15:00.000Z
+    def check_datetime_str(cls, time):
+        parse(time)
+        return time
+
+class UserProfile(BaseModel):
+    cookie: str = ""
+    views: List[UserTag] = []
+    buys: List[UserTag] = []
+
+class Statistics(BaseModel):
+    columns: List[str] = []
+    rows: List[List[str]] = []
+
+class TimeRange:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    start: datetime
+    end: datetime
+
+###############################################################################
+
+def parse_time_range(time_range: str):
+    #TODO: validate
+    parts = time_range.split('_', 1)
+    return TimeRange(parse(parts[0]), parse(parts[1]))
