92d05e2d4b22459f3ac3c30be80c0e1d3dc44492
Jagoda Kamińska <jk406179@students.mimuw.edu.pl>
2022-09-15 02:11:56 +0200

    kafka

--- common/config.py
+++ common/config.py
@@ -1,5 +1,5 @@
-kafka_hosts = ['st105vm106.rtb-lab.com:9092']
-aerospike_hosts = [('st105vm102.rtb-lab.com', 3000)]
+kafka_hosts = ['st105vm106.rtb-lab.com:9094']
+aerospike_hosts = [('st105vm103.rtb-lab.com', 3000)]
 kafka_topic = "user_tags"
 
 # #frontend_hosts/bucket_master_hosts <= #kafka_hosts/partitions
