4d94262fdb465e90a607c5b295e336a7eb9d4901
Jagoda Kamińska <jk406179@students.mimuw.edu.pl>
2022-09-15 11:09:11 +0200

    ansible fix

--- run.sh
+++ run.sh
@@ -2,13 +2,6 @@ echo "Install ansible and sshpass..."
 sudo apt -y install ansible sshpass
 echo "Done"
 
-echo "Install newest docker-compose..."
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-export PATH=~/usr/local/bin:$PATH
-docker-compose --version
-echo "Done"
-
 echo "Add other nodes to known_hosts:"
 for i in `seq -w 01 10`; do
     echo "Adding st105@st105vm1$i.rtb-lab.pl..."
@@ -18,7 +11,7 @@ done
 
 cd ansible
 
-echo "Setup docker and docker swarm on all nodes:"
+echo "Setup docker and docker-compose on all nodes:"
 ansible-playbook -i ./hosts --extra-vars "ansible_user=st105 ansible_password=$1" docker-playbook.yaml
 echo "Done"
 
